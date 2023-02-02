e4b142e2b6c5e811932dccd670bcbafb9f00b961
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2023-02-02 20:55:03 +0300

    Added Warrior

--- src/main/java/com/camunda/example/PrepareToBattle.java
+++ src/main/java/com/camunda/example/PrepareToBattle.java
@@ -1,8 +1,11 @@
 package com.camunda.example;
 
+import com.camunda.example.domain.Warrior;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -24,11 +27,26 @@ public class PrepareToBattle implements JavaDelegate {
             throw new BpmnError("warriorsError");
         }
 
-        List<Boolean> army = new ArrayList<>(Collections.nCopies(warriors, true));
+//        List<Boolean> army = new ArrayList<>(Collections.nCopies(warriors, true));
+        List<Warrior> army = new ArrayList<>();
+
+        for (int i = 0; i < warriors; i++) {
+            army.add(createWarrior(i));
+        }
 
         System.out.println("enemyWarriors army " + enemyWarriors + " vs our army " + warriors);
-
+        ObjectValue armyJson = Variables.objectValue(army).serializationDataFormat("application/json").create();
         delegateExecution.setVariable("army", army);
+        delegateExecution.setVariable("armyJson", armyJson);
         delegateExecution.setVariable("enemyWarriors", enemyWarriors);
+    }
+
+    private Warrior createWarrior(int i) {
+        Warrior warrior = new Warrior();
+        warrior.setName("Ivan " + i);
+        warrior.setTitle("Title" + i);
+        warrior.setHp((int) (Math.random() * 1000));
+        warrior.setAlive(true);
+        return warrior;
     }
 }
